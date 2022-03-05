3fb92a9b1c93c8bfbcf36efd19c2bd8313e566e2
Hsu1997 <hanson20620@gmail.com>
2022-03-05 15:36:19 +0800

    220305

--- daily/740_Delete_and_Earn.cpp
+++ daily/740_Delete_and_Earn.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -7,21 +8,38 @@ class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
         int table[10001] = {0};
-        int point[10001] = {0};
+        int max_num = 0;
         for (int i : nums){
-            table[i]++;
+            table[i] += i;
+            max_num = max(i, max_num);
         }
-        for (int i = 1; i < 10001; i++){
-            table[i] *= i;
-            // cout << table[i] << " ";
+        // think which method would like to use
+        int back_one = 0;
+        int back_two = 0;
+        int size = nums.size();
+        int current;
+        if (max_num < size + size * log2(size)){
+            back_one = table[1];
+            for (int i = 2; i <= max_num; i++){
+                current = back_one;
+                back_one = max(table[i] + back_two, back_one);
+                back_two = current;
+            }
         }
-        // cout << endl;
-        point[1] = table[1];
-        for (int i = 2; i < 10001; i++){
-            point[i] = max(point[i-1], table[i] + point[i-2]);
-            // cout << point[i] << " ";
+        else{
+            vector<int> key = {};
+            for (int i = 0; i <= max_num; i++){
+                if (table[i] != 0) key.push_back(i);
+            }
+            sort(key.begin(), key.end());
+            back_one = table[key.at(0)];
+            for (int i = 1; i < key.size(); i++){
+                current = back_one;
+                back_one = max(table[key.at(i)] + back_two, back_one);
+                back_two = current;
+            }
         }
-        return point[10000];
+        return back_one;
     }
 };
 
