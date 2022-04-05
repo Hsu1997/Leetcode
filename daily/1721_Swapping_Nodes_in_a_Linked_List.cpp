ab18b6a5eb13cb47b7612bff17bd69b3ba59f6b9
Hsu1997 <hanson20620@gmail.com>
2022-04-05 15:49:20 +0800

    220404

--- daily/1721_Swapping_Nodes_in_a_Linked_List.cpp
+++ daily/1721_Swapping_Nodes_in_a_Linked_List.cpp
@@ -30,59 +30,6 @@ void PrintLinkList(ListNode* head){
     cout << endl;
 }
 
-// class Solution {
-// public:
-//     ListNode* swapNodes(ListNode* head, int k) {
-//         int count = 0;
-//         ListNode* C = head;
-//         while(C != nullptr){
-//             count++;
-//             C = C->next;
-//         }
-//         if (count == 0 || (count % 2 == 1 && k == count / 2 + 1)) return head;
-//         else if (count == 2){
-//             ListNode* second = head->next;
-//             second->next = head;
-//             head->next = nullptr;
-//             return second;
-//         }
-//         else if (k > count / 2) k = (count/2)+(count%2)-k%(count/2)+1;
-
-//         ListNode* pseudo = new ListNode(0, head);
-//         ListNode* first = pseudo;
-//         ListNode* find = head;
-        
-//         for (int i = 0; i < k - 1; i++){
-//             find = find->next;
-//             first = first->next;
-//         }
-
-//         ListNode* k_th = find;
-//         find = find->next;
-
-//         ListNode* k2_th = head;
-//         ListNode* second = pseudo;
-
-//         while(find != nullptr){
-//             find = find->next;
-//             k2_th = k2_th->next;
-//             second = second->next;
-//         }
-//         first->next = k2_th;
-//         first = k2_th->next;
-//         if (k_th != second){
-//             k2_th->next = k_th->next;
-//             second->next = k_th;
-//         }
-//         else{
-//             k2_th->next = k_th;
-//             k_th->next = first;
-//         }
-//         k_th->next = first;
-//         return head;
-//     }
-// };
-
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
