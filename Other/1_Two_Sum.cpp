6c9d53759a3e995957a3ef6a83645a7bb64381bf
Hsu1997 <hanson20620@gmail.com>
2022-04-06 16:18:39 +0800

    220406

---
+++ Other/1_Two_Sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans = {};
+        vector<pair<int,int>> sorted = {};
+        for (int i = 0; i < nums.size(); i++) sorted.push_back(make_pair(nums[i], i));
+        sort(sorted.begin(), sorted.end());
+        int start = 0;
+        int end = nums.size()-1;
+        while(start < end){
+            if (sorted[start].first + sorted[end].first == target) break;
+            else if (sorted[start].first + sorted[end].first > target) end--;
+            else start++;
+        }
+        ans.push_back(sorted[start].second);
+        ans.push_back(sorted[end].second);
+        return ans;
+    }
+};
+
+int main(){
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    Solution S;
+    vector<int> ans = S.twoSum(nums, target);
+    for (auto i : ans) cout << i << " ";
+    cout << endl;
+    return 0;
+}
