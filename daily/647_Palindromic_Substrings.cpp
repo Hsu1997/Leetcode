3b5142ca0737d683c1178fecf16838e6c7051ccf
Hsu1997 <hanson20620@gmail.com>
2022-05-22 14:17:17 +0800

    220522

---
+++ daily/647_Palindromic_Substrings.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        if (s.length() == 0) return 0;
+        int ans = 0;
+        // for all character being center, need to consider even and odd size string
+        for (int i = 0; i < s.length(); i++){
+            int j = i - 1; 
+            int k = i;
+            // for even size string
+            while(k < s.length()-1 && s[k] == s[k+1]) k++;
+            // k is the last element with same character
+            ans += (k - j)*(k - j + 1) / 2;
+            // after loop, i will plus 1
+            i = k;
+            k++;
+            while(j >= 0 && k < s.length() && s[j] == s[k]){
+                ans++;
+                j--;
+                k++;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    string s = "abc";
+    Solution S;
+    cout << S.countSubstrings(s) << endl;
+    return 0;
+}
