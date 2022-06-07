aee377428655f348b76e758b92f357769636ab2a
Hsu1997 <hanson20620@gmail.com>
2022-06-07 12:21:23 +0800

    220525

--- daily/354_Russian_Doll_Envelopes.cpp
+++ daily/354_Russian_Doll_Envelopes.cpp
@@ -33,6 +33,9 @@ public:
         // }
         // return ans;
         
+
+        // reference :
+        // https://leetcode.com/problems/longest-increasing-subsequence/discuss/1326552/Optimization-From-Brute-Force-to-Dynamic-Programming-Explained!
         int len = 0;
         for (auto ele : envelopes){
             int cur = ele[1];
