309bb6f78d4f76054a22fc364127e09872828128
Hsu1997 <hanson20620@gmail.com>
2022-08-08 11:05:10 +0800

    220807

--- daily/1220_Count_Vowels_Permutation.cpp
+++ daily/1220_Count_Vowels_Permutation.cpp
@@ -7,19 +7,20 @@ class Solution {
 public:
     int countVowelPermutation(int n) {
         int mod = 1e9+7;
-        vector<vector<int>> dp(n+1, vector<int>(5,0));
-        dp[0] = {1,1,1,1,1};
+        int a = 1, e = 1, i = 1, o = 1, u = 1;
+        int an = 1, en = 1, in = 1, on = 1, un = 1;
 
-        for (int i = 1; i < n; i++){
-            dp[i][0] = ((dp[i-1][1] + dp[i-1][2]) % mod + dp[i-1][4] % mod) % mod;
-            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod;
-            dp[i][2] = (dp[i-1][1] + dp[i-1][3]) % mod;
-            dp[i][3] = dp[i-1][2] % mod;
-            dp[i][4] = (dp[i-1][2] + dp[i-1][3]) % mod;
+        for (int k = 1; k < n; k++){
+            an = ((e + i) % mod + u % mod) % mod;
+            en = (a + i) % mod;
+            in = (e + o) % mod;
+            on = i;
+            un = (i + o) % mod;
+
+            a = an, e = en, i = in, o = on, u = un;
         }
 
-        int ans = 0;
-        for (int i = 0; i < 5; i++) ans = (ans + dp[n-1][i]) % mod;
+        int ans = (((((a + e) % mod) + i) % mod + o) % mod + u) % mod;
         return ans;
     }
 };
