f7bb743d2d00f5c85005c7052ece3f2deef3474b
Hsu1997 <hanson20620@gmail.com>
2022-11-11 17:44:02 +0800

    221110

---
+++ daily/1047_Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        if (s.length() < 2) return s;
+
+        string ans;
+        for (auto i : s){
+            if (ans.back() == i) ans.pop_back();
+            else ans += i; 
+        }
+        
+        return ans;
+    }
+};
+
+int main(){
+    Solution S;
+    string s = "abbaca";
+    // string s = "azxxzy";
+    cout << S.removeDuplicates(s) << endl;
+    return 0;
+}
