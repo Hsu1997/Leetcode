d30e2e4aed427ec14961254ef55411288460c89a
Hsu1997 <hanson20620@gmail.com>
2023-01-10 13:50:35 +0800

    230110

---
+++ daily/100_Same_Tree.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* create(vector<int> nodes){
+    if (nodes.empty()) return nullptr;
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> que;
+    que.push(root);
+    int i = 1;
+    while(i < nodes.size() && !que.empty()){
+        TreeNode* temp = que.front();
+        que.pop();
+        if (i < nodes.size() && nodes[i] != -1){
+            temp->left = new TreeNode(nodes[i]);
+            que.push(temp->left);    
+        }
+        i++;
+        if (i < nodes.size() && nodes[i] != -1){
+            temp->right = new TreeNode(nodes[i]);
+            que.push(temp->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+int main(){
+    vector<int> p_nodes = {1,2,3};
+    vector<int> q_nodes = {1,2,3};
+    // vector<int> p_nodes = {1,2};
+    // vector<int> q_nodes = {1,-1,2};
+    // vector<int> p_nodes = {1,2,1};
+    // vector<int> q_nodes = {1,1,2};
+    TreeNode* p = create(p_nodes);
+    TreeNode* q = create(q_nodes);
+    Solution S;
+    cout << S.isSameTree(p, q) << endl;
+    return 0;
+}
