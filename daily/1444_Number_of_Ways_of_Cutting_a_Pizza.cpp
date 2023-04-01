aa88a63a1162eefa8e775da06a6e8d2f60117391
Hsu1997 <hanson20620@gmail.com>
2023-04-01 19:12:49 +0800

    230331

--- daily/1444_Number_of_Ways_of_Cutting_a_Pizza.cpp
+++ daily/1444_Number_of_Ways_of_Cutting_a_Pizza.cpp
@@ -72,66 +72,16 @@ public:
     }
 };
 
-class ASolution {
-public:
-    int ways(vector<string>& pizza, int k) {
-        int rows = pizza.size(), cols = pizza[0].size();
-        vector apples(rows + 1, vector<int>(cols + 1));
-        vector dp(k, vector(rows, vector<int>(cols)));
-        for (int row = rows - 1; row >= 0; row--) {
-            for (int col = cols - 1; col >= 0; col--) {
-                apples[row][col] = (pizza[row][col] == 'A') + apples[row + 1][col] +
-                                   apples[row][col + 1] - apples[row + 1][col + 1];
-                dp[0][row][col] = apples[row][col] > 0;
-            }
-        }
-        const int mod = 1000000007;
-        for (int remain = 1; remain < k; remain++) {
-            for (int row = 0; row < rows; row++) {
-                for (int col = 0; col < cols; col++) {
-                    for (int next_row = row + 1; next_row < rows; next_row++) {
-                        if (apples[row][col] - apples[next_row][col] > 0) {
-                            (dp[remain][row][col] += dp[remain - 1][next_row][col]) %= mod;
-                        }
-                    }
-                    for (int next_col = col + 1; next_col < cols; next_col++) {
-                        if (apples[row][col] - apples[row][next_col] > 0) {
-                            (dp[remain][row][col] += dp[remain - 1][row][next_col]) %= mod;
-                        }
-                    }
-                }
-            }
-        }
-        cout << "correct answer : " << endl;
-        for (auto i : dp){
-            for (auto j : i){
-                for (auto k : j){
-                    cout << k << " ";
-                }
-                cout << endl;
-            }
-            cout << endl;
-        }
-        return dp[k - 1][0][0];
-    }
-};
-
 int main(){
     // vector<string> pizza = {"A..","AAA","..."};
+    // int k = 3;
     // vector<string> pizza = {"A..","AA.","..."};
     // int k = 3;
     // vector<string> pizza = {"A..","A..","..."};
     // int k = 1;
-
-    vector<string> pizza = {".A..A",
-                            "A.A..",
-                            "A.AA.",
-                            "AAAA.",
-                            "A.AA."};
+    vector<string> pizza = {".A..A","A.A..","A.AA.","AAAA.","A.AA."};
     int k = 5;
     Solution S;
     cout << S.ways(pizza, k) << endl;
-    // ASolution S1;
-    // cout << S1.ways(pizza, k) << endl;
     return 0;
 }
