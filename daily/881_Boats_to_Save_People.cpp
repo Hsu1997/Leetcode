52160b821eb82ecc65baf90e7e49eea676f0695f
Hsu1997 <hanson20620@gmail.com>
2023-04-05 18:26:54 +0800

    230403

--- daily/881_Boats_to_Save_People.cpp
+++ daily/881_Boats_to_Save_People.cpp
@@ -22,46 +22,11 @@ public:
 };
 
 int main(){
-    vector<int> people = {6,5,4,2,6};
-    // people.erase(people.begin());
-    // for (auto i : people) cout << i << " ";
-    // cout << endl;
-    int limit = 6;
+    vector<int> people = {1,2};
+    int limit = 3;
+    // vector<int> people = {6,5,4,2,6};
+    // int limit = 6;
     Solution S;
     cout << S.numRescueBoats(people, limit) << endl;
     return 0;
 }
-
-// class Solution {
-// public:
-//     int numRescueBoats(vector<int>& people, int limit) {
-//         sort(people.begin(), people.end());
-//         reverse(people.begin(), people.end());
-        
-//         // for (auto i : people) cout << i << " ";
-//         // cout << endl;
-//         vector<int> ans = {};
-//         vector<int>::iterator ptr;
-//         for (vector<int>::iterator i = people.begin(); i != people.end(); i++){
-//             cout << "now is : " << *i << " ";
-//             if (ans.empty()){
-//                 ans.push_back(*i);
-//                 cout << "push " << *i << endl;
-//                 continue;
-//             }
-//             for (ptr = ans.begin(); ptr != ans.end(); ptr++){
-//                 if (*ptr + *i > limit) continue;
-//                 else{
-//                     *ptr += *i;
-//                     cout << "plus" << endl;
-//                     break;
-//                 }
-//             }
-//             if (ptr == ans.end()){
-//                 ans.push_back(*i);
-//                 cout << "push " << *i << endl;
-//             } 
-//         }
-//         return ans.size();;
-//     }
-// };
