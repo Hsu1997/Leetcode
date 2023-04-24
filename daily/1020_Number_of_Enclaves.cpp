ddf16d4a8908413cd01fe4f79232375d993ecda9
Hsu1997 <hanson20620@gmail.com>
2023-04-24 17:34:10 +0800

    230406

--- daily/1020_Number_of_Enclaves.cpp
+++ daily/1020_Number_of_Enclaves.cpp
@@ -14,7 +14,7 @@ public:
     void dfs(vector<vector<int>>& grid, int i, int j){
         if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) return;
         grid[i][j] = 0;
-        land++;
+        land--;
         for (int d = 0; d < 4; d++){
             dfs(grid, i + xd[d], j + yd[d]);
         }
@@ -26,7 +26,7 @@ public:
         n = grid[0].size();
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                if (grid[i][j] == 1) land--;
+                if (grid[i][j] == 1) land++;
             }
         }
         for (int i = 0; i < m; i++){
@@ -37,13 +37,13 @@ public:
             dfs(grid, 0, j);
             dfs(grid, m-1, j);
         }
-        return -land;
+        return land;
     }
 };
 
 int main(){
-    // vector<vector<int>> grid = {{0,0,0,0},{1,0,1,0},{0,1,1,0},{0,0,0,0}};
-    vector<vector<int>> grid = {{0,1,1,0},{0,0,1,0},{0,0,1,0},{0,0,0,0}};
+    vector<vector<int>> grid = {{0,0,0,0},{1,0,1,0},{0,1,1,0},{0,0,0,0}};
+    // vector<vector<int>> grid = {{0,1,1,0},{0,0,1,0},{0,0,1,0},{0,0,0,0}};
     Solution S;
     cout << S.numEnclaves(grid) << endl;
     return 0;
