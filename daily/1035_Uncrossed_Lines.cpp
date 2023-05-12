b45ba8bb9c96aa3652c190e66310fc6b7a19d06d
Hsu1997 <hanson20620@gmail.com>
2023-05-12 18:32:16 +0800

    230511

--- daily/1035_Uncrossed_Lines.cpp
+++ daily/1035_Uncrossed_Lines.cpp
@@ -8,36 +8,31 @@ public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        vector<vector<int>> dp(m, vector<int>(n,0));
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (i == 0 || j == 0){
-                    if (i == j) dp[i][j] = (nums1[i] == nums2[j]);
-                    else if (nums1[i] == nums2[j]) dp[i][j] = 1;
-                    else if (i == 0) dp[i][j] = dp[i][j-1];
-                    else if (j == 0) dp[i][j] = dp[i-1][j];
-                }
-                else{
-                    if (nums1[i] == nums2[j]) dp[i][j] = dp[i-1][j-1] + 1;
-                    else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
+        vector<vector<int>> dp(2, vector<int>(n+1,0));
+        for (int j = 1; j <= n; j++) dp[1][j] = dp[1][j-1] || (nums1[0] == nums2[j-1]);
+        for (int i = 2; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (nums1[i-1] == nums2[j-1]) dp[i%2][j] = dp[(i+1)%2][j-1] + 1;
+                else dp[i%2][j] = max(dp[(i+1)%2][j], dp[i%2][j-1]);
             }
         }
-        for (auto i : dp){
-            for (auto j : i) cout << j << " ";
-            cout << endl;
-        }
-        return dp[m-1][n-1];
+        // for (auto i : dp){
+        //     for (auto j : i) cout << j << " ";
+        //     cout << endl;
+        // }
+        return dp[m%2][n];
     }
 };
 
 int main(){
     // vector<int> nums1 = {1,4,2};
     // vector<int> nums2 = {1,2,4};
-    // vector<int> nums1 = {2,5,1,2,5};
-    // vector<int> nums2 = {10,5,2,1,5,2};
-    vector<int> nums1 = {1,3,7,1,7,5};
-    vector<int> nums2 = {1,9,2,5,1};
+    vector<int> nums1 = {2,5,1,2,5};
+    vector<int> nums2 = {10,5,2,1,5,2};
+    // vector<int> nums1 = {1,3,7,1,7,5};
+    // vector<int> nums2 = {1,9,2,5,1};
+    // vector<int> nums1 = {2};
+    // vector<int> nums2 = {3,1,1,3,3};
     Solution S;
     cout << S.maxUncrossedLines(nums1, nums2) << endl;
     return 0;
