f8f8a10032a6a9cd72188916863d234bbb3e888e
Hsu1997 <hanson20620@gmail.com>
2023-08-21 14:46:13 +0800

    230821

---
+++ daily/459_Repeated_Substring_Pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool subprob(string& s, int l){
+        for (int i = l; i < s.length(); i++){
+            if (s[i] != s[i%l]) return false;
+        }
+        return true;
+    }
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for (int i = 1; i < n; i++){
+            if (n % i == 0 && s[0] == s[i] && subprob(s, i)) return true; 
+        }
+        return false;
+    }
+};
+
+int main(){
+    // string s = "abab";
+    // string s = "aba";
+    string s = "abcabcabcabc";
+    Solution S;
+    cout << S.repeatedSubstringPattern(s) << endl;
+    return 0;
+}
