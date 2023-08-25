4cbaa80fde74ead57761d13bd08d913037773c76
Hsu1997 <hanson20620@gmail.com>
2023-08-25 18:36:12 +0800

    230825

--- daily/2023/97_Interleaving_String.cpp
+++ daily/2023/97_Interleaving_String.cpp
@@ -7,31 +7,33 @@ using namespace std;
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
+        if (s2.length() > s1.length()) swap(s1,s2);
         int l1 = s1.length();
         int l2 = s2.length();
         int l3 = s3.length();
         if (l3 != l1 + l2) return false;
-        vector<vector<bool>> dp(l1+1, vector<bool>(l2+1, false));
-        dp[0][0] = 1;
-        for (int j = 1; j < l2+1; j++) dp[0][j] = dp[0][j-1] && (s2[j-1] == s3[j-1]);
-        for (int i = 1; i < l1+1; i++) dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+        if (l1 == 0 || l2 == 0) return s1 == s3 || s2 == s3;
+        vector<bool> dp(l2+1, false);
+        dp[0] = 1;
+        for (int j = 1; j < l2+1; j++) dp[j] = dp[j-1] && (s2[j-1] == s3[j-1]);
+        // for (auto i : dp) cout << i << " ";
+        // cout << endl;
         for (int i = 1; i < l1+1; i++){
+            dp[0] = dp[0] && (s1[i-1] == s3[i-1]);
             for (int j = 1; j < l2+1; j++){
-                dp[i][j] = ((s3[i+j-1] == s1[i-1]) && dp[i-1][j]) || ((s3[i+j-1] == s2[j-1]) && dp[i][j-1]);
+                dp[j] = ((s3[i+j-1] == s1[i-1]) && dp[j]) || ((s3[i+j-1] == s2[j-1]) && dp[j-1]);
             }
+            // for (auto i : dp) cout << i << " ";
+            // cout << endl;
         }
-        // for (auto i : dp){
-        //     for (auto j : i) cout << j << " ";
-        //     cout << endl;
-        // }
-        return dp.back().back();
+        return dp.back();
     }
 };
 
 int main(){
-    string s1 = "aabcc";
-    string s2 = "dbbca";
-    string s3 = "aadbbcbcac";
+    string s1 = "aabaac";
+    string s2 = "aadaaeaaf";
+    string s3 = "aadaaeaabaafaac";
     Solution S;
     cout << S.isInterleave(s1,s2,s3) << endl;
     return 0;
