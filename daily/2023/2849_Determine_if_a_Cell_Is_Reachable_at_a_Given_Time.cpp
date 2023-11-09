db268e20ce8c726e23edb9cc6e25ab2156864fd4
Hsu1997 <hanson20620@gmail.com>
2023-11-09 16:26:43 +0800

    231108

--- daily/2023/2849_Determine_if_a_Cell_Is_Reachable_at_a_Given_Time.cpp
+++ daily/2023/2849_Determine_if_a_Cell_Is_Reachable_at_a_Given_Time.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class Solution {
 public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        if (sx == fx && sy == fy && t == 1) return false;
         return max(abs(fx-sx), abs(fy-sy)) <= t;
     }
 };
@@ -21,6 +22,11 @@ int main(){
     // int fx = 7;
     // int fy = 3;
     // int t = 3;
+    // int sx = 2;
+    // int sy = 1;
+    // int fx = 2;
+    // int fy = 1;
+    // int t = 1;
     Solution S;
     cout << S.isReachableAtTime(sx, sy, fx, fy, t) << endl;
     return 0;
