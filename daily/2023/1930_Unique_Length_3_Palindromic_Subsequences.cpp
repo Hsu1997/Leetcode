0402b6827eb1378850550bcef07cd6c88d931d6b
Hsu1997 <hanson20620@gmail.com>
2023-11-14 13:49:18 +0800

    231114

---
+++ daily/2023/1930_Unique_Length_3_Palindromic_Subsequences.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int ans = 0;
+        for (int i = 0; i < 26; i++){
+            char temp = 'a' + i;
+            int l = 0;
+            int r = s.length();
+            while(l < r && s[l] != temp) l++;
+            if (l == r) continue;
+            while(r >= 0 && s[r] != temp) r--;
+            if (r < 0) continue;
+            if (l == r) continue;
+            unordered_set<char> m;
+            for (int k = l+1; k < r; k++) m.insert(s[k]);
+            ans += m.size();
+        }
+        return ans;
+    }
+};
+
+int main(){
+    // string s = "aabca";
+    // string s = "adc";
+    string s = "bbcbaba";
+    Solution S;
+    cout << S.countPalindromicSubsequence(s) << endl;
+    return 0;
+}
