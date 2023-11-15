c96f7a7bd5701fb0e2b8681aa8dea60509f11ff4
Hsu1997 <hanson20620@gmail.com>
2023-11-15 18:36:32 +0800

    231104

--- daily/2023/1503_Last_Moment_Before_All_Ants_Fall_Out_of_a_Plan.cpp
+++ daily/2023/1503_Last_Moment_Before_All_Ants_Fall_Out_of_a_Plan.cpp
@@ -14,9 +14,15 @@ public:
 };
 
 int main(){
-    int n = 4;
-    vector<int> left = {4,3};
-    vector<int> right = {0,1};
+    // int n = 4;
+    // vector<int> left = {4,3};
+    // vector<int> right = {0,1};
+    // int n = 7;
+    // vector<int> left = {};
+    // vector<int> right = {0,1,2,3,4,5,6,7};
+    int n = 7;
+    vector<int> left = {0,1,2,3,4,5,6,7};
+    vector<int> right = {};
     Solution S;
     cout << S.getLastMoment(n, left, right) << endl;
     return 0;
