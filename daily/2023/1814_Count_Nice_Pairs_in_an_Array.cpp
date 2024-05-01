626cad3d9ad80f56e4793a256bed6f57bde0f316
Hsu1997 <hanson20620@gmail.com>
2024-05-01 16:38:11 +0800

    240501

---
+++ daily/2023/1814_Count_Nice_Pairs_in_an_Array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int countNicePairs(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+    vector<int> nums = {42,11,1,97};
+    // vector<int> nums = {13,10,35,24,76};
+    Solution S;
+    cout << S.countNicePairs(nums) << endl;
+    return 0;
+}
