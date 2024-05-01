55e893f6f7d665dd3c8b84eb686f94116bab85bb
Hsu1997 <hanson20620@gmail.com>
2024-05-01 16:38:11 +0800

    240501

---
+++ daily/2024/2000_Reverse_Prefix_of_Word.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        
+    }
+};
+
+int main(){
+    string word = "abcdefd";
+    char ch = 'd';
+    Solution S;
+    cout << S.reversePrefix(word, ch) << endl;
+    return 0;
+}
