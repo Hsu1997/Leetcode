19a81cb162a5b1cd09f5eea566b4528579f1694e
Hsu1997 <hanson20620@gmail.com>
2024-05-02 00:35:31 +0800

    240501

--- daily/2024/2000_Reverse_Prefix_of_Word.cpp
+++ daily/2024/2000_Reverse_Prefix_of_Word.cpp
@@ -7,13 +7,24 @@ using namespace std;
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        
+        string ans;
+        string::iterator ptr = word.begin();
+        for (; ptr != word.end(); ptr++){
+            if (*ptr == ch) break;
+        }
+        if (ptr == word.end()) return word;
+        reverse(word.begin(), ptr+1);
+        return word;
     }
 };
 
 int main(){
     string word = "abcdefd";
     char ch = 'd';
+    // string word = "xyxzxe";
+    // char ch = 'z';
+    // string word = "abcd";
+    // char ch = 'z';
     Solution S;
     cout << S.reversePrefix(word, ch) << endl;
     return 0;
