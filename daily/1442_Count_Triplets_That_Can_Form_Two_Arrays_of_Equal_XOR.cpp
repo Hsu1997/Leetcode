f825aa62e55bed2afb5f375478ef2e7d168132f6
Hsu1997 <hanson20620@gmail.com>
2024-05-30 19:20:21 +0800

    240530

--- daily/1442_Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR.cpp
+++ daily/1442_Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR.cpp
@@ -10,19 +10,13 @@ public:
         int n = arr.size();
         if (n < 2) return 0;
         int ans = 0;
-        vector<unordered_map<int,int>> m(n);
-        vector<int> acc_arr(n);
-        acc_arr[0] = arr[0];
-        for (int i = 1; i < n; i++) acc_arr[i] = acc_arr[i-1] ^ arr[i];
-        // for (auto i : acc_arr) cout << i << " ";
-        // cout << endl;
-        m[0][arr[0]]++;
-        for (int i = 1; i < n; i++){
-            m[i][acc_arr[i]]++;
-            for (int j = 0; j < i; j++){
-                m[i][acc_arr[i]^acc_arr[j]]++;
-                ans += m[j][acc_arr[i]^acc_arr[j]];
-            }
+        int prefix = 0;
+        unordered_map<int,int> count, total;
+        count[0]++;
+        for (int i = 0; i < arr.size(); i++){
+            prefix ^= arr[i];
+            ans += count[prefix]++ * i - total[prefix];
+            total[prefix] += (i + 1);
         }
         return ans;
     }
