c6775e12bef9188353382f3fd4d8c8903309d17b
Hsu1997 <hanson20620@gmail.com>
2024-06-01 14:48:32 +0800

    240601

---
+++ daily/3110_Score_of_a_String.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans = 0;
+        for (int i = 1; i < s.length(); i++){
+            ans += abs(s[i] - s[i-1]);
+        }
+        return ans;
+    }
+};
+
+int main(){
+    // string s = "hello";
+    string s = "aza";
+    Solution S;
+    cout << S.scoreOfString(s) << endl;
+    return 0;
+}
