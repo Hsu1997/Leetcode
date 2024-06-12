46268ef0ef0d5f8b83f9638b691757dcea0f409e
Hsu1997 <hanson20620@gmail.com>
2024-06-12 16:13:03 +0800

    240612

--- daily/2024/75_Sort_Colors.cpp
+++ daily/2024/75_Sort_Colors.cpp
@@ -6,17 +6,13 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int zero = 0;
-        int one = 0;
-        for (int i : nums){
-            if (i == 0) zero++;
-            if (i == 1) one++;
-        }
-        for (int i = 0; i < n; i++){
-            if (zero-- > 0) nums[i] = 0;
-            else if (one-- > 0) nums[i] = 1;
-            else nums[i] = 2;
+        int white = 0;
+        int red = 0;
+        int blue = nums.size()-1;
+        while(white <= blue){
+            if (nums[white] == 0) swap(nums[white++], nums[red++]);
+            else if (nums[white] == 1) white++;
+            else swap(nums[white], nums[blue--]);
         }
     }
 };
