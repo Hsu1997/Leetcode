5acf49a0a397c6e426b7cfbf28675f1ca11d689b
Hsu1997 <hanson20620@gmail.com>
2024-08-06 18:49:26 +0800

    240806

---
+++ daily_python/3016_Minimum_Number_of_Pushes_to_Type_Word_II.py
@@ -0,0 +1,41 @@
+import sys
+import os
+from typing import List
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        frequency = [0] * 26
+        for c in word:
+            frequency[ord(c) - ord('a')] += 1
+        frequency.sort(reverse=True)
+        ans = 0
+        for i in range(26):
+            if frequency[i] == 0:
+                return ans
+            else:
+                ans += (i // 8 + 1 ) * frequency[i]
+        return ans
+    
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        data = content.split('\n')
+        for ex in data:
+            ex = ex.split('=')[1].strip().strip(';').strip('\"')
+            dataset.append(ex)
+    return dataset
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1):
+        path = os.path.basename(__file__)
+        filename = os.path.splitext(path)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    results = []
+    solution = Solution()
+    dataset = readDataSet(filename)
+    for example in dataset:
+        results.append(solution.minimumPushes(example))
+    for index, result in enumerate(results):
+        print(f'Example {index+1} : {result}')
