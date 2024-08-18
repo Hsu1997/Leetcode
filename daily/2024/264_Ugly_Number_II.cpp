14c0625831889c2066bf0f6123a4998dbb4be4a0
Hsu1997 <hanson20620@gmail.com>
2024-08-18 16:05:51 +0800

    240818

--- daily/2024/264_Ugly_Number_II.cpp
+++ daily/2024/264_Ugly_Number_II.cpp
@@ -8,22 +8,22 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         if (n == 1) return 1;
-        priority_queue<long long, vector<long long>, greater<long long>> two;
-        priority_queue<long long, vector<long long>, greater<long long>> three;
-        priority_queue<long long, vector<long long>, greater<long long>> five;
+        queue<long long> two;
+        queue<long long> three;
+        queue<long long> five;
         two.push(2);
         three.push(3);
         five.push(5);
         for (int i = 2; i < n; i++){
-            long long temp = min({two.top(), three.top(), five.top()});
-            if (two.top() == temp) two.pop();
-            if (three.top() == temp) three.pop();
-            if (five.top() == temp) five.pop();
+            long long temp = min({two.front(), three.front(), five.front()});
+            if (two.front() == temp) two.pop();
+            if (three.front() == temp) three.pop();
+            if (five.front() == temp) five.pop();
             two.push(temp * 2);
             three.push(temp * 3);
             five.push(temp * 5);
         }
-        return min({two.top(), three.top(), five.top()});
+        return min({two.front(), three.front(), five.front()});
     }
 };
 
