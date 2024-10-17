8de8e33fd17483b9e6b88bd839219ec8afc4f1b4
Hsu1997 <hanson20620@gmail.com>
2024-10-17 20:18:02 +0800

    240928

--- Contest/Biweekly Contest 140/3303_Find_the_Occurrence_of_First_Almost_Equal_Substring.cpp
+++ Contest/Biweekly Contest 140/3303_Find_the_Occurrence_of_First_Almost_Equal_Substring.cpp
@@ -1,49 +1,74 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_set>
 #include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    int minStartingIndex(string s, string pattern) {
-        int m = s.length();
-        int n = pattern.length();
-        if (m < n) return -1;
-        unordered_map<char,int> curr;
-        unordered_map<char,int> target;
-        for (char c : pattern) target[c]++;
-        int match = 0;
-        for (int i = 0; i < n; i++){
-            char temp = s[i];
-            curr[temp]++;
-            if (curr[temp] <= target[temp]) match++;
+    vector<int> z_function(string& s) {
+        int n = s.size();
+        vector<int> z(n);
+        int l = 0, r = 0;
+        for(int i = 1; i < n; i++) {
+            if(i < r) {
+                z[i] = min(r - i, z[i - l]);
+            }
+            while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+                z[i]++;
+            }
+            if(i + z[i] > r) {
+                l = i;
+                r = i + z[i];
+            }
         }
-        cout << match << endl;
-        if (match >= n-1) return 0;
-        for (int i = n; i < m; i++){
-            char added = s[i];
-            char del = s[i - n];
-            curr[added]++;
-            if (curr[added] <= target[added]) match++;
-            curr[del]--;
-            if (curr[del] < target[del]) match--;
-            if (match >= n-1) return i - n + 1;
+        return z;
+    }
+    int minStartingIndex(string s, string pattern) {
+        int n = s.length();
+        int m = pattern.length();
+        if (n < m) return -1;
+        string prefix = pattern + "#" + s;
+        reverse(pattern.begin(), pattern.end());
+        reverse(s.begin(), s.end());
+        string suffix = pattern + "#" + s;
+        vector<int> pre = z_function(prefix);
+        // cout << prefix << endl;
+        // for (int i : pre) cout << i;
+        // cout << endl;
+        vector<int> suf = z_function(suffix);
+        // cout << suffix << endl;
+        // for (int i : suf) cout << i;
+        // cout << endl;
+        pre = vector<int>(pre.begin() + m + 1, pre.end());
+        suf = vector<int>(suf.begin() + m + 1, suf.end());
+        reverse(suf.begin(), suf.end());
+        // for (int i : pre) cout << i;
+        // cout << endl;
+        // for (int i : suf) cout << i;
+        // cout << endl;
+        for (int i = 0; i + m - 1 < n; i++){
+            // cout << i << " : " << pre[i] << "+" << suf[i+m-1] << "+" << 1 << endl;
+            if (pre[i] + suf[i+m-1] + 1 >= m) return i;
         }
         return -1;
     }
+
 };
 
 int main(){
-    // string s = "abcdefg";
-    // string pattern = "bcdffg";
-    string s = "ababbababa";
-    string pattern = "bacaba";
+    string s = "abcdefg";
+    string pattern = "bcdffg";
+    // string s = "ababbababa";
+    // string pattern = "bacaba";
     // string s = "abcd";
     // string pattern = "dba";
     // string s = "dde";
     // string pattern = "d";
+    // string s = "eeedededeedeedeeedeedddededeeddeedeedeeddededdddddedde";
+    // string pattern = "deeddee";
     Solution S;
     cout << S.minStartingIndex(s, pattern) << endl;
     return 0;
