3e744fd8724c67b25e5798c220824e99767af577
Hsu1997 <hanson20620@gmail.com>
2024-10-18 14:17:19 +0800

    241017

--- daily_python/670_Maximum_Swap.py
+++ daily_python/670_Maximum_Swap.py
@@ -12,11 +12,8 @@ class Solution:
             for d in range(9, int(num_str[i]), -1):
                 if last_seen[d] > i:
                     num_str[i], num_str[last_seen[d]] = (num_str[last_seen[d]], num_str[i])
-                    
                     num_str = "".join(num_str)
-                    
                     return int(num_str)
-                    
         return num
     
 # class Solution:
