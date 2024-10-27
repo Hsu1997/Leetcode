c9f8c32c93e391df5f9df1c524c331ca91a63be5
Hsu1997 <hanson20620@gmail.com>
2024-10-27 19:04:28 +0800

    241027

--- daily_python/1277_Count_Square_Submatrices_with_All_Ones.py
+++ daily_python/1277_Count_Square_Submatrices_with_All_Ones.py
@@ -7,7 +7,6 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         dp = [[0] * (n+1) for _ in range(m+1)]
-        # print(dp)
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 1:
