330c734bc0e2cc6837da462c164c0d19c69dd3b1
Hsu1997 <hanson20620@gmail.com>
2024-11-06 16:42:18 +0800

    241105

---
+++ daily_python/2914_Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+class Solution:
+    def minChanges(self, s: str) -> int:
+        ans = 0
+        n = len(s)
+        for i in range(n//2):
+            if s[i*2] != s[i*2+1]:
+                ans += 1
+        return ans
+    
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            s = lines[0].split('=')[1].strip()[1:-2]
+            dataset.append(s)
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for s in dataset:
+        results.append(solution.minChanges(s))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
