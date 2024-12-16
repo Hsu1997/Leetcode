3260bcdc93c997dafe674299335065ecb99b50e2
Hsu1997 <hanson20620@gmail.com>
2024-12-16 21:00:40 +0800

    241210

--- daily/2024/2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I.cpp
+++ daily/2024/2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I.cpp
@@ -7,27 +7,34 @@ using namespace std;
 class Solution {
 public:
     int maximumLength(string s) {
-        vector<vector<int>> count(26, vector<int>(53, 0));
-        int l = s.length();
+        int n = s.length();
+        vector<vector<int>> count(26, vector<int>(n+1, 0));
         char prev = s[0];
         int curr_len = 1;
-        int ans = 0;
-        for (int i = 1; i < l; i++){
+        count[prev-'a'][curr_len] = 1;
+        int ans = -1;
+        for (int i = 1; i < n; i++){
             char curr = s[i];
-            if (curr == prev) curr_len++;
+            if (curr == prev){
+                curr_len++;
+                count[curr-'a'][curr_len]++;
+            }
             else{
-                count[prev-'a'][curr_len]++;
                 prev = curr;
                 curr_len = 1;
+                count[prev-'a'][curr_len] += 1;
             }
         }
-        count[s[l-1]-'a'][curr_len]++;
-        for (int c = 50; c >= 1; c--){
-            for (int i = 0; i < 26; i++){
-                if (count[i][c] >= 3 || count[i][c] && count[i][c+1] || count[i][c+1] >= 2 || count[i][c+2]) return c;
+        for (int i = 0; i < 26; i++){
+            for (int l = n-1; l > 0; l--){
+                count[i][l] += count[i][l+1];
+                if (count[i][l] >= 3){
+                    ans = max(ans, l);
+                    break;
+                }
             }
         }
-        return (ans == 0)? -1 : ans;
+        return ans;
     }
 };
 
