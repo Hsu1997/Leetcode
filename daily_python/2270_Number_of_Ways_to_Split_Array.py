7c509b40fda68223f343b1c43326a79b5a31c4b7
Hsu1997 <hanson20620@gmail.com>
2025-01-03 15:54:38 +0800

    250103

---
+++ daily_python/2270_Number_of_Ways_to_Split_Array.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from typing import List
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        left = 0
+        right = sum(nums)
+        ans = 0
+        for i in range(len(nums)-1):
+            left += nums[i]
+            right -= nums[i]
+            if left >= right:
+                ans += 1
+        return ans
+    
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            nums = list(map(int, lines[0].split('=')[1].strip()[1:-2].split(',')))
+            dataset.append(nums)
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for nums in dataset:
+        results.append(solution.waysToSplitArray(nums))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
