68ba8fced5f553fbc7aa8726ebb0762b9ad647fb
Hsu1997 <hanson20620@gmail.com>
2025-01-12 20:21:18 +0800

    250112

---
+++ daily/2025/2116_Check_if_a_Parentheses_String_Can_Be_Valid.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.length();
+        if (n % 2) return false;
+        int open = 0;
+        int close = 0;
+        for (int i = 0; i < n; i++){
+            if (locked[i] == '1'){
+                if (s[i] == '(') open++;
+                else close++;
+            }
+        }
+        if (open > n/2 || close > n/2) return false;
+        int change = n/2 - open;
+        for (int i = 0; i < n; i++){
+            if (locked[i] == '0'){
+                if (change){
+                    s[i] = '(';
+                    change--;
+                }
+                else s[i] = ')';
+            }
+        }
+        int acc = 0;
+        for (int i = 0; i < n; i++){
+            if (s[i] == '('){
+                acc++;
+            }
+            else{
+                acc--;
+                if (acc < 0) return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(){
+    // string s = "))()))";
+    // string locked = "010100";
+    // string s = "()()";
+    // string locked = "0000";
+    string s = ")";
+    string locked = "0";
+    Solution S;
+    cout << S.canBeValid(s, locked) << endl;
+    return 0;
+}
