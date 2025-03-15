579275a703a4ee432c255c66ce8191cb88c84ec1
Hsu1997 <hanson20620@gmail.com>
2025-03-15 16:14:42 +0800

    250119

--- daily/2025/407_Trapping_Rain_Water_II.cpp
+++ daily/2025/407_Trapping_Rain_Water_II.cpp
@@ -50,8 +50,8 @@ public:
 };
 
 int main(){
-    // vector<vector<int>> heightMap = {{1,4,3,1,3,2},{3,2,1,3,2,4},{2,3,3,2,3,1}};
-    vector<vector<int>> heightMap = {{3,3,3,3,3},{3,2,2,2,3},{3,2,1,2,3},{3,2,2,2,3},{3,3,3,3,3}};
+    vector<vector<int>> heightMap = {{1,4,3,1,3,2},{3,2,1,3,2,4},{2,3,3,2,3,1}};
+    // vector<vector<int>> heightMap = {{3,3,3,3,3},{3,2,2,2,3},{3,2,1,2,3},{3,2,2,2,3},{3,3,3,3,3}};
     Solution S;
     cout << S.trapRainWater(heightMap) << endl;
     return 0;
