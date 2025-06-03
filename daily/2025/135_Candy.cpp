7f496e45261297b5ee8c77c81c680292dbc71925
Hsu1997 <hanson20620@gmail.com>
2025-06-03 18:19:41 +0800

    250602

---
+++ daily/2025/135_Candy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> ans(n, 1);
+        for (int i = 1; i < n; i++){
+            if (ratings[i] > ratings[i-1]) ans[i] = ans[i-1] + 1;
+        }
+        for (int i = n - 2; i >= 0; i--){
+            if (ratings[i] > ratings[i+1]) ans[i] = max(ans[i], ans[i+1] + 1);
+        }
+        int total = 0;
+        for (int i : ans) total += i;
+        return total;
+    }
+};
+
+int main(){
+    vector<int> ratings = {1,0,2};
+    // vector<int> ratings = {1,2,2};
+    Solution S;
+    cout << S.candy(ratings) << endl;
+    return 0;
+}
