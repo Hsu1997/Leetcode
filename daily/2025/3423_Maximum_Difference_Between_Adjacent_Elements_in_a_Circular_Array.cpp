5327d61f4beadea0253c20596ce97cc03844a275
Hsu1997 <hanson20620@gmail.com>
2025-06-12 17:27:11 +0800

    250612

---
+++ daily/2025/3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+        int n = nums.size();
+        int ans = abs(nums[n-1] - nums[0]);
+        for (int i = 0; i < n-1; i++){
+            ans = max(ans, abs(nums[i] - nums[i+1]));
+        }
+        return ans;
+    }
+};
+
+int main(){
+    vector<int> nums = {1,2,4};
+    // vector<int> nums = {-5,-10,-5};
+    Solution S;
+    cout << S.maxAdjacentDistance(nums) << endl;
+    return 0;
+}
