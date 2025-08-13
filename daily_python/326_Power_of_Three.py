6838d5143047f0712c0f391fefda662538bce367
Hsu1997 <hanson20620@gmail.com>
2025-08-13 20:42:01 +0800

    250813

---
+++ daily_python/326_Power_of_Three.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 1:
+            return False
+        while n > 1:
+            if n % 3 > 0:
+                return False
+            n //= 3
+        return True
+    
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            n = int(lines[0].split('=')[1].strip()[:-1])
+            dataset.append(n)
+    return dataset
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1):
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for n in dataset:
+        results.append(solution.isPowerOfThree(n))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
