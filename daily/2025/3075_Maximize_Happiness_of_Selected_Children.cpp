1066f2f42f84091c4b9239501a229b5fe0039a05
Hsu1997 <hanson20620@gmail.com>
2025-12-28 22:56:40 +0800

    251225

--- daily/2025/3075_Maximize_Happiness_of_Selected_Children.cpp
+++ daily/2025/3075_Maximize_Happiness_of_Selected_Children.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         sort(happiness.begin(), happiness.end(), greater<int>());
-        int ans = 0;
+        long long ans = 0;
         for (int i = 0; i < k; i++){
             ans += max(0, happiness[i] - i);
         }
