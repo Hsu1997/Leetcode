64be69291f0d0c79c2a233d6273c9907bc10f461
Hsu1997 <hanson20620@gmail.com>
2025-12-28 21:06:43 +0800

    251228

---
+++ daily_python/1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from typing import List
+
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] < 0:
+                    ans += n - j
+                    break
+        return ans
+
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            grid = [list(map(int, row.split(','))) for row in lines[0].split('=')[1].strip()[2:-3].split('},{')]
+            dataset.append(grid)
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for grid in dataset:
+        results.append(solution.countNegatives(grid))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
+    
