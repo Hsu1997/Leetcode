4ae57f2309a12ed20006099f03e4d4e732bfcdd0
Hsu1997 <hanson20620@gmail.com>
2026-01-03 23:56:31 +0800

    260103

--- daily/2026/1411_Number_of_Ways_to_Paint_N×3_Grid.cpp
+++ daily/2026/1411_Number_of_Ways_to_Paint_N×3_Grid.cpp
@@ -7,26 +7,36 @@ class Solution {
 public:
     int numOfWays(int n) {
         int mod = 1e9 + 7;
+        // a : xyx, b : xyz
+        long long a = 6;
+        long long b = 6;
         long long ans = 0;
-        vector<long long> dp(12, 1);
         for (int i = 2; i <= n; i++){
-            vector<long long> temp(12, 0);
-            temp[0] = (dp[4] + dp[5] + dp[7] + dp[8] + dp[9]) % mod;
-            temp[1] = (dp[4] + dp[6] + dp[7] + dp[8]) % mod;
-            temp[2] = (dp[4] + dp[5] + dp[8] + dp[9] + dp[11]) % mod;
-            temp[3] = (dp[5] + dp[9] + dp[10] + dp[11]) % mod;
-            temp[4] = (dp[0] + dp[1] + dp[2] + dp[10] + dp[11]) % mod;
-            temp[5] = (dp[0] + dp[2] + dp[3] + dp[10]) % mod;
-            temp[6] = (dp[1] + dp[8] + dp[9] + dp[11]) % mod;
-            temp[7] = (dp[0] + dp[1] + dp[9] + dp[10] + dp[11]) % mod;
-            temp[8] = (dp[0] + dp[1] + dp[2] + dp[6]) % mod;
-            temp[9] = (dp[0] + dp[2] + dp[3] + dp[6] + dp[7]) % mod;
-            temp[10] = (dp[3] + dp[4] + dp[5] + dp[7]) % mod;
-            temp[11] =  (dp[2] + dp[3] + dp[4] + dp[6] + dp[7]) % mod;
-            dp = temp;
+            long long nextA = (a * 3 + b * 2) % mod;
+            long long nextB = (a * 2 + b * 2) % mod;
+            a = nextA;
+            b = nextB;
         }
-        for (long long i : dp) ans = (ans + i) % mod;
-        return ans;
+        return (a + b) % mod;
+        // vector<long long> dp(12, 1);
+        // for (int i = 2; i <= n; i++){
+        //     vector<long long> temp(12, 0);
+        //     temp[0] = (dp[4] + dp[5] + dp[7] + dp[8] + dp[9]) % mod;
+        //     temp[1] = (dp[4] + dp[6] + dp[7] + dp[8]) % mod;
+        //     temp[2] = (dp[4] + dp[5] + dp[8] + dp[9] + dp[11]) % mod;
+        //     temp[3] = (dp[5] + dp[9] + dp[10] + dp[11]) % mod;
+        //     temp[4] = (dp[0] + dp[1] + dp[2] + dp[10] + dp[11]) % mod;
+        //     temp[5] = (dp[0] + dp[2] + dp[3] + dp[10]) % mod;
+        //     temp[6] = (dp[1] + dp[8] + dp[9] + dp[11]) % mod;
+        //     temp[7] = (dp[0] + dp[1] + dp[9] + dp[10] + dp[11]) % mod;
+        //     temp[8] = (dp[0] + dp[1] + dp[2] + dp[6]) % mod;
+        //     temp[9] = (dp[0] + dp[2] + dp[3] + dp[6] + dp[7]) % mod;
+        //     temp[10] = (dp[3] + dp[4] + dp[5] + dp[7]) % mod;
+        //     temp[11] =  (dp[2] + dp[3] + dp[4] + dp[6] + dp[7]) % mod;
+        //     dp = temp;
+        // }
+        // for (long long i : dp) ans = (ans + i) % mod;
+        // return ans;
     }
 };
 
