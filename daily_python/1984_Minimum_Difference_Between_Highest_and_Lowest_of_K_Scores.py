1389d035656727b8683d2746ba9cb2c9921e4829
Hsu1997 <hanson20620@gmail.com>
2026-01-25 15:44:26 +0800

    260125

---
+++ daily_python/1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from typing import List
+
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        ans = float('inf')
+        n = len(nums)
+        nums.sort()
+        for i in range(n + 1 - k):
+            ans = min(ans, nums[i + k - 1] - nums[i])
+        return ans
+
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            nums = list(map(int, lines[0].split('=')[1].strip()[1:-2].split(',')))
+            k = int(lines[1].split('=')[1].strip()[:-1])
+            dataset.append((nums, k))
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for nums, k in dataset:
+        results.append(solution.minimumDifference(nums, k))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
