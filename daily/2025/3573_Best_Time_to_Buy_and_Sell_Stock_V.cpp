ad75fe3f1b10db8707c8381e340bc2cdf4428afc
Hsu1997 <hanson20620@gmail.com>
2026-01-29 15:32:36 +0800

    251217

--- daily/2025/3573_Best_Time_to_Buy_and_Sell_Stock_V.cpp
+++ daily/2025/3573_Best_Time_to_Buy_and_Sell_Stock_V.cpp
@@ -8,21 +8,36 @@ public:
     long long maximumProfit(vector<int>& prices, int k) {
         int n = prices.size();
         // [idx][transaction][empty: 0, short: 1, long: 2]
-        vector<vector<vector<long long>>> dp(n, vector<vector<long long>>(k + 1, vector<long long>(3, -1e9)));
-        dp[0][0][0] = 0;
-        dp[0][0][1] = prices[0];
-        dp[0][0][2] = -prices[0];
+        // vector<vector<vector<long long>>> dp(n, vector<vector<long long>>(k + 1, vector<long long>(3, -1e9)));
+        // dp[0][0][0] = 0;
+        // dp[0][0][1] = prices[0];
+        // dp[0][0][2] = -prices[0];
+        // for (int i = 1; i < n; i++){
+            //     for (int j = 0; j <= k; j++){
+                //         dp[i][j][0] = dp[i-1][j][0];
+                //         if (j > 0) dp[i][j][0] = max({dp[i][j][0], dp[i-1][j-1][1] - prices[i], dp[i-1][j-1][2] + prices[i]});
+                //         dp[i][j][1] = max(dp[i-1][j][0] + prices[i], dp[i-1][j][1]);
+                //         dp[i][j][2] = max(dp[i-1][j][0] - prices[i], dp[i-1][j][2]);
+                //     }
+                // }
+                // long long ans = 0;
+                // for (int j = 0; j <= k; j++) ans = max(ans, dp[n-1][j][0]);
+            // return ans;
+
+        // [idx][transaction][empty: 0, short: 1, long: 2]
+        vector<vector<long long>> dp(k + 1, vector<long long>(3, 0));
+        for (int j = 0; j <= k; j++){
+            dp[j][1] = prices[0];
+            dp[j][2] = -prices[0];
+        }
         for (int i = 1; i < n; i++){
-            for (int j = 0; j <= k; j++){
-                dp[i][j][0] = dp[i-1][j][0];
-                if (j > 0) dp[i][j][0] = max({dp[i][j][0], dp[i-1][j-1][1] - prices[i], dp[i-1][j-1][2] + prices[i]});
-                dp[i][j][1] = max(dp[i-1][j][0] + prices[i], dp[i-1][j][1]);
-                dp[i][j][2] = max(dp[i-1][j][0] - prices[i], dp[i-1][j][2]);
+            for (int j = k; j > 0; j--){
+                dp[j][2] = max(dp[j][2], dp[j][0] - prices[i]);
+                dp[j][1] = max(dp[j][1], dp[j][0] + prices[i]);
+                dp[j][0] = max({dp[j][0], dp[j-1][1] - prices[i], dp[j-1][2] + prices[i]});
             }
         }
-        long long ans = 0;
-        for (int j = 0; j <= k; j++) ans = max(ans, dp[n-1][j][0]);
-        return ans;
+        return dp[k][0];
     }
 };
 
