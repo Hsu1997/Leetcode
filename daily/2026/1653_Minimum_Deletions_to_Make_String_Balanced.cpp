92343cd829ec72e8c3f2534c6c72668b7ffa8040
Hsu1997 <hanson20620@gmail.com>
2026-02-07 15:56:03 +0800

    260207

--- daily/2026/1653_Minimum_Deletions_to_Make_String_Balanced.cpp
+++ daily/2026/1653_Minimum_Deletions_to_Make_String_Balanced.cpp
@@ -7,20 +7,16 @@ using namespace std;
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
-        vector<int> cnta(n);
-        vector<int> cntb(n);
-        cnta[0] = s[0] == 'a';
-        for (int i = 1; i < n; i++){
-            cnta[i] = cnta[i-1] + (s[i] == 'a');
-        }
-        cntb[n-1] = s[n-1] == 'b';
-        for (int i = n - 2; i >= 0; i--){
-            cntb[i] = cntb[i+1] + (s[i] == 'b');
-        }
+        // use stack to count 'ba' pair appeared times
+        int cntb = 0;
         int ans = 0;
-        for (int i = 0; i < n; i++) ans = max(ans, cnta[i] + cntb[i]);
-        return n - ans;
+        for (char c : s){
+            if (c == 'a'){
+                if (cntb > 0) cntb--, ans++;
+            }
+            else cntb++;
+        }
+        return ans;
     }
 };
 
