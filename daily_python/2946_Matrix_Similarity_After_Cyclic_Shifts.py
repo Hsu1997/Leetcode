606f92549e20bc1667715518802179b6bd653f5c
Hsu1997 <hanson20620@gmail.com>
2026-03-27 22:55:47 +0800

    260327

---
+++ daily_python/2946_Matrix_Similarity_After_Cyclic_Shifts.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from typing import List
+
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        m, n = len(mat), len(mat[0])
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != mat[i][(j + k) % n]:
+                    return False
+        return True
+    
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            mat = [list(map(int, row.split(','))) for row in lines[0].split('=')[1].strip()[2:-3].split('},{')]
+            k = int(lines[1].split('=')[1].strip()[:-1])
+            dataset.append((mat, k))
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for mat, k in dataset:
+        results.append(solution.areSimilar(mat, k))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
