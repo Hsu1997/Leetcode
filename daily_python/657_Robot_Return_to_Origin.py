c2a0222e2573a6ce72f08d341c2610e9faffe706
Hsu1997 <hanson20620@gmail.com>
2026-04-05 15:41:29 +0800

    260405

---
+++ daily_python/657_Robot_Return_to_Origin.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')
+
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            moves = lines[0].split('=')[1].strip()[1:-2]
+            dataset.append(moves)
+    return dataset
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1):
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for moves in dataset:
+        results.append(solution.judgeCircle(moves))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
