60aa5c11123ecdde68494e65988637ad01c9e65b
Hsu1997 <hanson20620@gmail.com>
2026-05-04 20:21:56 +0800

    260504

---
+++ daily_python/48_Rotate_Image.py
@@ -0,0 +1,40 @@
+import sys
+import os
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+        return
+
+
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            matrix = [list(map(int, row.split(','))) for row in lines[0].split('=')[1].strip()[2:-3].split('},{')]
+            dataset.append(matrix)
+    return dataset
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1):
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for matrix in dataset:
+        solution.rotate(matrix)
+        results.append(matrix)
+    for index, result in enumerate(results):
+        print(f'Example {index+1} : {result}')
