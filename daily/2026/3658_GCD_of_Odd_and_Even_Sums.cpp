2cca8a713fc6ae2b4100e32c358a0374986473c5
Hsu1997 <hanson20620@gmail.com>
2026-07-15 22:19:36 +0800

    260715

---
+++ daily/2026/3658_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main(){
+    int n = 4;
+    // int n = 5;
+    Solution S;
+    cout << S.gcdOfOddEvenSums(n) << endl;
+    return 0;
+}
