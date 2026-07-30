8b74d85d998d71e62ed1b5bec18e48ab58172178
Hsu1997 <hanson20620@gmail.com>
2026-07-30 15:18:10 +0800

    260730

---
+++ daily_python/3014_Minimum_Number_of_Pushes_to_Type_Word_I.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from typing import List
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n = len(word)
+        m = (n - 1) // 8 + 1
+        return 4 * m * (m - 1) + (n - 8 * (m - 1)) * m
+    
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            word = lines[0].split('=')[1].strip()[1:-2]
+            dataset.append(word)
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for word in dataset:
+        results.append(solution.minimumPushes(word))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
