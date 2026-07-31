262d81b7cbad1402ab22ef8c97656c77f95558c5
Hsu1997 <hanson20620@gmail.com>
2026-07-31 17:15:51 +0800

    260731

---
+++ daily/2026/3016_Minimum_Number_of_Pushes_to_Type_Word_II.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> cnt(26, 0);
+        for (char c : word) cnt[c - 'a']++;
+        sort(cnt.begin(), cnt.end(), greater<int>());
+        int ans = 0;
+        for (int i = 0; i < cnt.size(); i++){
+            ans += cnt[i] * (i / 8 + 1);
+        }
+        return ans;
+    }
+};
+
+int main(){
+    string word = "abcde";
+    // string word = "xyzxyzxyzxyz";
+    // string word = "aabbccddeeffgghhiiiiii";
+    Solution S;
+    cout << S.minimumPushes(word) << endl;
+    return 0;
+}
