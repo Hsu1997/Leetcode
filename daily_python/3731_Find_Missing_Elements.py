0e78f3da418370a401ee63d301255f556bc656e3
Hsu1997 <hanson20620@gmail.com>
2026-08-04 15:02:34 +0800

    260804

---
+++ daily_python/3731_Find_Missing_Elements.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from typing import List
+
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        st = set(nums)
+        M, m = max(nums), min(nums)
+        ans = [x for x in range(m + 1, M) if x not in st]
+        return ans
+    
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            nums = list(map(int, lines[0].split('=')[1].strip()[1:-2].split(',')))
+            dataset.append(nums)
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for nums in dataset:
+        results.append(solution.findMissingElements(nums))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
