29e3ea4fd11913d73a2284c5e1da44f41cd30ec9
Hsu1997 <hanson20620@gmail.com>
2026-09-05 18:53:26 +0800

    260905

---
+++ daily_python/3904_Smallest_Stable_Index_II.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from typing import List
+
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        res = [0] * n
+        res[n-1] = nums[n-1]
+        for i in range(n-2, -1, -1):
+            res[i] = min(res[i+1], nums[i])
+        M = 0
+        for i in range(n):
+            M = max(M, nums[i])
+            if M - res[i] <= k:
+                return i
+        return -1
+
+def readDataSet(filename):
+    dataset = []
+    with open(filename, 'r') as file:
+        content = file.read().strip()
+        blocks = content.split('\n\n')
+        for block in blocks:
+            lines = block.split('\n')
+            nums = list(map(int, lines[0].split('=')[1].strip()[1:-2].split(',')))
+            k = int(lines[1].split('=')[1].strip()[:-1])
+            dataset.append((nums, k))
+    return dataset
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        filename = os.path.splitext(__file__)[0] + '.txt'
+    else:
+        filename = sys.argv[1]
+    dataset = readDataSet(filename)
+    results = []
+    solution = Solution()
+    for nums, k in dataset:
+        results.append(solution.firstStableIndex(nums, k))
+    for index, result in enumerate(results):
+        print(f'Example {index + 1} : {result}')
